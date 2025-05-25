013688ce2b9b03dcb3f11d0c48e5c553ff7dc4e7
sabamacharashvili <smach9627@gmail.com>
2025-05-25 13:34:43 +0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,30 @@
+function GetData() {
+  fetch("https://jsonplaceholder.typicode.com/posts")
+    .then((response) => response.json())
+    .then((data) => {
+      data.forEach((post) => {
+        createElement(post.title, post.body);
+      });
+    })
+
+    .catch((err) => console.log(err));
+}
+const container = document.getElementById("container");
+const createElement = (titletext, bodytext) => {
+  const card = document.createElement("div");
+  const title = document.createElement("h5");
+  const body = document.createElement("p");
+
+  card.classList.add("card");
+  title.classList.add("title");
+  body.classList.add("body");
+
+  container.append(card);
+  card.append(title);
+  card.append(body);
+
+  title.innerHTML = titletext;
+  body.innerHTML = bodytext;
+};
+
+GetData();
